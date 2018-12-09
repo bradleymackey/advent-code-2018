31a7dbf17e7219234c592081880d9b22d6f264f2
Bradley Mackey <me@bradleymackey.com>
2018-12-09 18:29:12 +0000

    better algorithm for day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,46 +1,41 @@
 # advent of code day 5
 # bradleymackey
 
-# both challenges
-import re
 
-def remove_pair(s, i):
-    return s[:i] + s[i+2:]
+
+# both challenges
 
 def compact_polymer_removing_char(package):
 	"""
 	calculate the polymer deduction, removing a char beforehand (pass '' to remove no char)
-	parameter should be a tuple (string,char), where string is the initial string and char is the char to remove
+	parameter should be a tuple (`string`,`char`), where `string` is the initial string and `char` is the char to remove
 	"""
+	# better method inspired by: https://www.reddit.com/r/adventofcode/comments/a3912m/2018_day_5_solutions/eb4jzni/
+	# i was too caught up to realise this is in no way whatsoever a >O(n^2) problem, which is what I made it
 	string = package[0]
 	char = package[1]
-	lower = str(char).lower()
-	upper = str(char).upper()
-	new = re.sub(lower+"|"+upper,"",string)
-	# print("now trying:",char,"string:",new)
-	end = False
-	while end==False:
-		end = True
-		for i in range(len(new)-1):
-			index_reached = i
-			char_one = str(new[i])
-			char_two = str(new[i+1])
-			if char_one.lower()==char_two.lower():
-				if (char_one.isupper() and char_two.islower()) or (char_one.islower() and char_two.isupper()):
-					new = remove_pair(new,i)
-					if len(new)%10000 == 0:
-						print("[UPDATE] current",char,"length is now",len(new))
-					end = False
-					break
-	print(char,"result length is is",len(new))
-	return len(new)
+	stack = ['-']
+	for c in string:
+		j = stack[-1]
+		if c.lower() == char:
+			continue
+		elif c != j and j.lower() == c.lower():
+			stack.pop()
+		else:
+			stack.append(c)
+	return len(stack) - 1
 
 
-# challenge 1
+from string import ascii_lowercase
+import itertools
+from multiprocessing import Pool
+
 with open('input.txt', 'r') as file:
 
 	# scan left to right, as per the docs
-	# this can take a few mins, depending on computer specs
+	# this should be very fast to run now!
+
+	# ----- challenge 1 -----
 
 	# strip the input string or risk an off-by-one error!
 	# <very annoying>
@@ -50,36 +45,25 @@ with open('input.txt', 'r') as file:
 	answer = compact_polymer_removing_char((string,''))
 	print("challenge 1 answer:",answer)
 
+	# ----- challenge 2 -----
 
-
-# challenge 2
-import itertools
-from multiprocessing import Pool
-with open('input.txt', 'r') as file:
-
-	# this will take longer to run!
-	# multithreading should help though!
+	# multithreading to way speed this up!
 	# thanks to https://stackoverflow.com/a/28463266/3261161
-	# (p.s. i found that the answer seems to pop out before the others have finished...)
-	# (...so you can probably terminate early as soon as that first one is found)
-
-	string = file.readline().strip()
-	char_list = "abcdefghijklmnopqrstuvwxyz"
 
 	# make the Pool of workers
 	# ... giving each char its own thread
-	pool = Pool(len(char_list))
+	pool = Pool(len(ascii_lowercase))
 
 	# give each character it's own thread, also passing in the original string as well
 	# and return the results
-	results = pool.map(compact_polymer_removing_char, zip(itertools.repeat(string), char_list))
+	results = pool.map(compact_polymer_removing_char, zip(itertools.repeat(string), ascii_lowercase))
 
 	# close the pool and wait for the work to finish 
 	pool.close() 
 	pool.join()
 
 	# zip each char back with its result
-	result_data = zip(char_list,results)
+	result_data = zip(ascii_lowercase,results)
 		
 	best_char = None
 	best_len = None
@@ -87,4 +71,4 @@ with open('input.txt', 'r') as file:
 		if best_len==None or length<best_len:
 			best_len = length
 			best_char = char
-	print("challenge 2 answer:",best_len,"(from char '"+best_char+"'')")
+	print("challenge 2 answer:",best_len,"(from char '"+best_char+"')")
